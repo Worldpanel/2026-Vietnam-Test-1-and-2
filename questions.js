139c98cad8a8eb412497a186ef6cef5d84aea36d
Worldpanel <vananh8695@gmail.com>
2026-02-26 01:08:11 +0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -344,12 +344,12 @@ window.QUESTION_BANK = [
     text: `
       <strong>Directions:</strong><br />
       The following questions consist of two quantities, one in Column A and one in Column B.<br />
-      The two quantities are to be compared and you are to choose:<br /><br />
+      The two quantities are to be compared and you are to choose:<br />
       A. if the quantity in Column A is greater<br />
       B. if the quantity in Column B is greater<br />
       C. if the two quantities are the same<br />
-      D. if the comparison cannot be made based on the information given<br /><br />
-      <strong>Tick the correct answer.</strong><br /><br />
+      D. if the comparison cannot be made based on the information given<br />
+      <strong>Tick the correct answer.</strong><br />
       <div style="background:#eee; padding:5px 8px; line-height:1.25; border-radius:5px;">
         <strong>Column A:</strong> (1/4) / (3/8) &nbsp;&nbsp;|&nbsp;&nbsp; <strong>Column B:</strong> 1 1/2
       </div>
